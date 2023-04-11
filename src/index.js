7b84dd45fe8ac92ddd73f89ca3b0732e04662813
Victoria <victoriyaboyarkina@gmail.com>
2023-04-11 20:06:01 +0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+import fs  from 'fs';;
+import process from 'process';
+import path from 'path';
 
 const gendiff = (filepath1, filepath2) => {
 
 };
+
+const getPath = (filepath) => {
+	const directory = process.cwd();
+	const fullPath = path.resolve(directory, filepath);
+	return fullPath;
+};
+  
+const getExtension = (filepath) => {
+	return path.extname(filepath);
+}
+  
+const readFile = (filepath) => {
+	const path = getPath(filepath);
+	fs.readFile(path, 'utf8', function(err, data) {
+		return data;
+	});
+}
 
 export default gendiff;
