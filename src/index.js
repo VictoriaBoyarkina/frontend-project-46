b09a0454a08536c78acbb9f06d17e1064452f61b
Victoria <victoriyaboyarkina@gmail.com>
2023-04-12 17:44:01 +0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,10 @@ const gendiff = (filepath1, filepath2) => {
 
 };
 
-const getPath = (filepath) => {
-	const directory = process.cwd();
-	const fullPath = path.resolve(directory, filepath);
-	return fullPath;
-};
-  
-const getExtension = (filepath) => {
-	return path.extname(filepath);
-}
-  
-const readFile = (filepath) => {
-	const path = getPath(filepath);
-	fs.readFile(path, 'utf8', function(err, data) {
-		return data;
-	});
-}
+const getPath = (filepath) => path.resolve(process.cwd(), path);
+
+const getExtension = (filepath) => path.extname(filepath).substring(1);
+
+const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8')
 
 export default gendiff;
