21fa173e86968025f4afaf7a4dbe78f08cf704af
Victoria <victoriyaboyarkina@gmail.com>
2023-04-19 22:06:54 +0700

    tests

---
+++ _tests_/gendiff.test.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@jest/globals';
+import gendiff from '../src/index.js';
+
+test('gendiff', () => {
+  expect(gendiff('./fixtures/file1.json', './fixtures/file2.json')).toEqual('{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true}');
+});
