4f5af6d5c1f05c48178233acb0889102b7844774
Victoria <victoriyaboyarkina@gmail.com>
2023-04-19 22:02:26 +0700

    delete .eslintrc.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import fs  from 'fs';
 import process from 'process';
 import path from 'path';
-import getObject from './parser.js'
+import getObject from './parser.js';
+import _ from 'lodash'
 
 
 const getPath = (filepath) => path.resolve(process.cwd(), filepath);
@@ -9,6 +10,25 @@ const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 const getExtension = (filepath) => path.extname(filepath).substring(1);
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
+
+const getListOfKeys = (file1, file2) => {
+	const list1 = Object.keys(file1);
+	const list2 = Object.keys(file2);
+	const listOfKeys = [...list1, ...list2];
+	const uniqArray = _.uniq(listOfKeys);
+	return uniqArray.sort();
+};
+
+const checkValue = (key, file1, file2) => {
+	if (file1[key] === file2[key]) {
+		return `    ${key}: ${file1[key]}`;
+	} else if (!Object.hasOwn(file2, key)) {
+		return `  - ${key}: ${file1[key]}`;
+	} else if (!Object.hasOwn(file1, key)) {
+		return `  + ${key}: ${file2[key]}`;
+	}
+	return (`  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`);
+};
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
 	const fullPath1 = getPath(filepath1);
@@ -19,8 +39,15 @@ const gendiff = (filepath1, filepath2, format = 'stylish') => {
 	const file2 = readFile(fullPath2);
 	const object1 = getObject(file1, extension1);
 	const object2 = getObject(file2, extension2);
+	const listOfKeys = getListOfKeys(object1, object2);
+	const result = [];
+	for (let i = 0; i < listOfKeys.length; i += 1) {
+		result.push(checkValue(listOfKeys[i], object1, object2))
+	}
+	console.log(`{\n${result.join('\n')}\n}`);
+
 };
 
-gendiff('./fixtures/file1.json', './fixtures/file1.json');
+gendiff('./fixtures/file1.json', './fixtures/file2.json');
 
 export default gendiff;
