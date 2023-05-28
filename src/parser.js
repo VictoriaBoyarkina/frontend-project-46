f86184005a817d866aaa02404fb8530081f79d86
Victoria <victoriyaboyarkina@gmail.com>
2023-05-28 18:01:34 +0700

    all

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,13 @@
+import yaml from 'js-yaml';
+
 const getObject = (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
+    case 'yml':
+      return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
     default:
       return JSON.parse(data);
   }
