35c006259966dd0874a95af6da3bb78a2d915f28
Victoria <victoriyaboyarkina@gmail.com>
2023-08-04 19:43:40 +0700

    all

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,5 @@
 import gendiff from '../src/index.js';
 
 test('gendiff1', () => {
-  expect(gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
-});
-
-test('gendiff2', () => {
-  expect(gendiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml')).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
-});
-
-test('gendiff3', () => {
-  expect(gendiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+  expect(gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual('./__fixtures__/result.txt');
 });
