c4326a2f495f95500f28a3b10936bed019c9f0aa
Victoria <victoriyaboyarkina@gmail.com>
2023-08-07 20:09:10 +0700

    correct test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,4 @@
-import gendiff from '../src/formatters/index.js';
+import gendiff from '../src/index.js';
 
 test('gendiff1', () => {
   expect(gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual('./__fixtures__/result.txt');
