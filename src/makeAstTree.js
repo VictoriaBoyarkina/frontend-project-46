d2ebfe7ba30932ee941dab5e5465fda34af8da26
Victoria <victoriyaboyarkina@gmail.com>
2023-08-18 20:59:35 +0700

    remove sort method from asttree

--- src/makeAstTree.js
+++ src/makeAstTree.js
@@ -1,12 +1,43 @@
 /* eslint-disable max-len */
 import _ from 'lodash';
 
+const bubbleSort = (coll) => {
+  let stepsCount = coll.length - 1;
+  // Объявляем переменную swapped, значение которой показывает,
+  // был ли совершен обмен элементов во время перебора массива
+  let swapped;
+  // do..while цикл. Работает почти идентично while
+  // Разница в проверке. Тут она делается не до выполнения тела, а после.
+  // Такой цикл полезен там, где надо выполнить тело хотя бы раз в любом случае.
+  do {
+    swapped = false;
+    // Перебираем массив и меняем местами элементы, если предыдущий
+    // больше, чем следующий
+    for (let i = 0; i < stepsCount; i += 1) {
+      if (coll[i] > coll[i + 1]) {
+        // temp – временная константа для хранения текущего элемента
+        const temp = coll[i];
+        coll[i] = coll[i + 1];
+        coll[i + 1] = temp;
+        // Если сработал if и была совершена перестановка,
+        // присваиваем swapped значение true
+        swapped = true;
+      }
+    }
+    // Уменьшаем счетчик на 1, т.к. самый большой элемент уже находится
+    // в конце массива
+    stepsCount -= 1;
+  } while (swapped); // продолжаем, пока swapped === true
+
+  return coll;
+};
+
 const getListOfKeys = (file1, file2) => {
   const list1 = Object.keys(file1);
   const list2 = Object.keys(file2);
   const listOfKeys = [...list1, ...list2];
   const uniqArray = _.uniq(listOfKeys);
-  return uniqArray.sort();
+  return bubbleSort(uniqArray);
 };
 
 const makeAstTree = (object1, object2) => {
