0c48a3c5e681b09004bbb888f8651c5778bfc6c0
Victoria <victoriyaboyarkina@gmail.com>
2023-08-26 12:01:03 +0700

    refractoring

--- src/makeAstTree.js
+++ src/makeAstTree.js
@@ -16,18 +16,12 @@ const makeAstTree = (object1, object2) => {
       if (_.isObject(object1[key]) && _.isObject(object2[key])) {
         return { key, type: 'nested', children: makeAstTree(object1[key], object2[key]) };
       }
-    }
-    if (_.has(object1, key) && _.has(object2, key)) {
       if (object1[key] === object2[key]) {
         return { key, type: 'unchanged', value: object1[key] };
       }
-    }
-    if (_.has(object1, key) && _.has(object2, key)) {
-      if (object1[key] !== object2[key]) {
-        return {
-          key, type: 'updated', value1: object1[key], value2: object2[key],
-        };
-      }
+      return {
+        key, type: 'updated', value1: object1[key], value2: object2[key],
+      };
     }
     if (_.has(object1, key) && !_.has(object2, key)) {
       return { key, type: 'removed', value: object1[key] };
